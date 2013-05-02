9be78b483ebe056be69b39643d9712b454bf66ae
J Leary <j.leary@yahoo.com>
2013-05-02 13:50:26 -0500

    Added a pages controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(
+	name: "A. Adams",
+	email: "a@user.com"
+	)
+
+User.create(
+	name: "B. Barker",
+	email: "b@user.com"
+	)
+
+User.create(
+	name: "C. Charles",
+	email: "c@user.com"
+	)
